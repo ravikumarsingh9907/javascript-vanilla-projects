882c40eb8199fc20d280a7adb375c0f2bec60068
Ravi Kumar <ravikumarsingh9907@gmail.com>
2021-10-11 14:17:44 +0530

    Update hexcolor.js

--- Color flipper/hexcolor.js
+++ Color flipper/hexcolor.js
@@ -1,17 +1,26 @@
 const hexcolor = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-const btn = document.getElementById('btn');
-const colors = document.querySelector('.colors');
 
-btn.addEventListener('click', function(){
-    let hexvalue = '#';
-    for(let i=0;i<6;i++){
-        hexvalue += hexcolor[randomColor()];
-    }
+const buttons = document.querySelectorAll(".btn");
+const value = document.querySelector(".value");
 
-    colors.textContent = hexvalue;
-    document.body.style.backgroundColor = hexvalue;
+buttons.forEach(function(ans){
+    ans.addEventListener('click', function(f){
+        let colorValue = '#';
+        // const RandomNumber = randomColor();
+        const styles = f.currentTarget.classList;
+        if(styles.contains('change')){
+            for(let i=0;i<6;i++){
+                colorValue += hexcolor[randomColor()]
+            }
+            value.textContent = colorValue;
+        }else if(styles.contains('reset')) {
+            document.body.style.backgroundColor = '#fff';
+            value.textContent = '#fff';
+        }
+        document.body.style.backgroundColor = colorValue;
+    });   
 });
 
 function randomColor(){
-    return Math.floor(Math.random()*hexcolor.length);
+    return Math.floor(Math.random() * hexcolor.length);
 }
