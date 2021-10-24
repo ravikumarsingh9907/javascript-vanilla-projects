3e0c5500f73469f03a06a93b9d4f03cc865dc1ec
Ravi Kumar <ravikumarsingh9907@gmail.com>
2021-10-24 12:29:34 +0530

    Create app.js

---
+++ Lorem Generator/app.js
@@ -0,0 +1,173 @@
+const loremText = [
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis facere
+    enim assumenda numquam nam nemo incidunt alias iusto minus, sed
+    provident iure necessitatibus perspiciatis? Soluta ad nisi ipsam
+    voluptate. Ea odit sunt adipisci fugiat aut beatae ipsa velit similique
+    blanditiis pariatur, non, illo eos quaerat! Tempora veritatis cum libero
+    maiores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+    sint eum temporibus, expedita illo sapiente, libero, modi natus
+    laboriosam ducimus quia. Debitis blanditiis possimus ea animi natus. Vel
+    veritatis neque at deleniti, nemo repellendus minima similique eum
+    culpa. Atque in ipsa iure a cupiditate accusamus enim itaque eligendi
+    cumque facilis?`,
+
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+    earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+    soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+    autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+    quod voluptate quia hic autem sequi odit quae non et totam provident
+    dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+    consequatur quidem tempore necessitatibus eius dicta illum fugit,
+    tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+    perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+    facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+    accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+    odit nesciunt laboriosam a quasi veniam ut, voluptates impedit esse.
+    Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+    quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+    neque laborum commodi incidunt, repellendus quo molestias quod odio
+    rerum quam quis accusantium optio. Sapiente, possimus qui?`,
+
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+        earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+        soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+        autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+        quod voluptate quia hic autem sequi odit quae non et totam provident
+        dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+        consequatur quidem tempore necessitatibus eius dicta illum fugit,
+        tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+        perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+        facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+        accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+        odit nesciunt laboriosam a quasi veniam ut, voluptates impedit esse.
+        Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+        quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+        neque laborum commodi incidunt, repellendus quo molestias quod odio
+        rerum quam quis accusantium optio. Sapiente, possimus qui?`,
+
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+        earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+        soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+        autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+        quod voluptate quia hic autem sequi odit quae non et totam provident
+        dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+        consequatur quidem tempore necessitatibus eius dicta illum fugit,
+        tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+        perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+        facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+        accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+        odit nesciunt laboriosam a quasi veniam ut, voluptates impedit esse.
+        Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+        quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+        neque laborum commodi incidunt, repellendus quo molestias quod odio
+        rerum quam quis accusantium optio. Sapiente, possimus qui? dlktfj dlfkejr sdfkljeo dflkjeoi dlefjoi`,
+
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+        earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+        soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+        autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+        quod voluptate quia hic autem sequi odit quae non et totam provident
+        dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+        consequatur quidem tempore necessitatibus eius dicta illum fugit,
+        tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+        perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+        facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+        accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+        odit nesciunt laboriosam a quasi veniam ut, voluptates impedit esse.
+        Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+        quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+        neque laborum commodi incidunt, repellendus quo molestias quod odio
+        rerum quam quis accusantium optio. Sapiente, possimus qui?fdjhjkbn dfgjndkgjr
+        vjkhnkjdfgn dfgjndijnvbk dfdkjnfkgjn  dfkjenvkjn dfkjgnkjsrfgn`,
+
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+        earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+        soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+        autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+        quod voluptate quia hic autem cgerfgn fghrg vgbrtg fgh fghrf gfh sequi odit quae non et totam provident
+        dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+        consequatur quidem tempore necessitatibus eius dicta illum fugit,
+        tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+        perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+        facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+        accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+        odit nesciunt laboriosam a q esse.
+        Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+        quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+        neque laborum commodi incidunt, repellendus quo molestias quod odio
+        rerum quam quis accusantium optio. Sapiente, possimus qui?`,
+    
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+        earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+        soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+        autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+        quod voluptate quia hic autem sequi odit quae non et totam provident
+        dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+        consequatur quidem tempore necessitatibus eius dicta illum fugit,
+        tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+        perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+        facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+        accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+        odit nesciunt laboriosam a quasi veniam ut, voluptates impedit esse.
+        Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+        quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+        neque laborum commodi incidunt, repellendus quo molestias quod odio
+        rerum quam quis accusantium optio. Sapiente, possimus qui?`,
+
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+        earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+        soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+        autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+        quod voluptate quia hic autem sequi odit quae non et totam provident
+        dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+        consequatur quidem tempore necessitatibus eius dicta illum fugit,
+        tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+        perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+        facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+        accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+        odit nesciunt laboriosam a quasi veniam ut, voluptates impedit esse.
+        Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+        quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+        neque laborum commodi incidunt, repellendus quo molestias quod odio
+        rerum quam quis accusantium optio. Sapiente, possimus qui?`,
+
+    `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima minus
+        earum repudiandae nihil laboriosam ex illo esse blanditiis, tempore
+        soluta ut quisquam qui, debitis ab nesciunt pariatur temporibus porro,
+        autem sunt distinctio? Ad vero nesciunt enim fugiat praesentium velit
+        quod voluptate quia hic autem sequi odit quae non et totam provident
+        dolorem deserunt, voluptates eius, quis rem. Ducimus temporibus omnis
+        consequatur quidem tempore necessitatibus eius dicta illum fugit,
+        tempora, eveniet est cupiditate non impedit ullam itaque voluptatem
+        perspiciatis obcaecati distinctio quam odit. Numquam fugit dolores, ex
+        facilis, accusamus minima velit sit magnam explicabo error, quibusdam
+        accusantium eius. Suscipit voluptas ducimus nobis! Pariatur asperiores
+        odit nesciunt laboriosam a quasi veniam ut, voluptates impedit esse.
+        Quae minus vitae debitis ducimus maiores provident eligendi labore aut
+        quia, mollitia quas temporibus nemo sapiente porro in atque! Repudiandae
+        neque laborum commodi incidunt, repellendus quo molestias quod odio
+        rerum quam quis accusantium optio. Sapiente, possimus qui?`
+];
+
+
+const form = document.querySelector('.form-content');
+const para = document.getElementById('input');
+const article = document.querySelector('.article-section');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = parseInt(para.value);
+    const random = Math.floor(Math.random()*loremText.length);
+
+    if(isNaN(value) || value <= 0 || value > 9) {
+        article.innerHTML = alert ("Sorry! for the inconvenience, it can only generate paragraphs 1 to 9");
+    }
+    else {
+        let Text = loremText.slice(0, value);
+        Text = Text.map(function(item) {
+            return `<p class="result">${item}</p>`;
+        }).join("");
+
+        article.innerHTML = Text;
+    }
+});
